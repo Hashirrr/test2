9430da4bfa243febb8718eab72e11bed825869ea
smallT123456 <shaffinmalik9@gmail.com>
2024-03-24 15:28:54 +0500

    Test6

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .api.serializers import CardSerializers  # Assuming the serializers file is in the same directory
+
+from .models import Card  # Assuming the models file is in the parent directory
+
+@api_view(['GET'])
+def card_list(request):
+    if request.method == 'GET':
+        cards = Card.objects.all()  # Retrieve all Card objects from the database
+        serializer = CardSerializers(cards, many=True)  # Serialize the queryset
+        return Response(serializer.data, status=status.HTTP_200_OK)
