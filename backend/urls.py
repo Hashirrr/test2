e603d067e43ed9b7aa8e1f73520e44cf533de9a4
smallT123456 <shaffinmalik9@gmail.com>
2024-03-24 15:58:33 +0500

    Test7

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
-
+from ..myapp import views
 def index(request):
     return HttpResponse('Hello')
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('backend.api.urls')),
     path('', index),
+    path('cards', views.card_list, name='yes'),
 ]
